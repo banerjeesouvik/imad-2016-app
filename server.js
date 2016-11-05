c1cf3a3b77fac9803746b21ac24e1d74a6a31f7a
Souvik Banerjee <banerjeesouvik@users.noreply.github.com>
2016-11-05 22:25:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,22 +16,24 @@ var app = express();
 app.use(morgan('combined'));
 
 function createTemplate(data){
-    console.log(data.length);
-    var finalTemplate="<body>";
-    for(var i=0;i<data.lenght;i++){
-    var title=data[i].title;
-    var poet=data[i].poet;
-    var dop=data[i].dop;
-    var body=data[i].body;
+    //console.log(data.length);
+    //var finalTemplate="<body>";
+    //for(var i=0;i<data.lenght;i++){
+    var title=data.title;
+    var poet=data.poet;
+    var dop=data.dop;
+    var body=data.body;
     var template= 
-                `<h2> ${title} <h2>
+                `<html>
+                 <h2> ${title} <h2>
                  <h3> Author: ${poet} , Posted On: ${dop} <h3>
                  <br>
                  <pre> ${body} <pre>
-                 <hr>`;
-    finalTemplate=finalTemplate.concat(template);
-    }
-    return finalTemplate;
+                 <hr>
+                 </html>`;
+    //finalTemplate=finalTemplate.concat(template);
+    //}
+    return template;
 }
 
 app.get('/', function (req, res) {
@@ -57,8 +59,8 @@ app.get('/test-db2', function (req, res) {
           res.status(500).send(err.toString());
       }
       else{
-          console.log(5+6);
-          //res.send(createTemplate(result.rows));
+          //console.log(5+6);
+          res.send(createTemplate(result.rows[0]));
       }
   });
 });
