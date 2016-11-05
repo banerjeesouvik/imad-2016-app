67d48adfab1f1b400db473ee4d4f713547c7936f
Souvik Banerjee <banerjeesouvik@users.noreply.github.com>
2016-11-05 18:14:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ app.get('/test-db2', function (req, res) {
           res.status(500).send(err.toString());
       }
       else{
+          console.log('data found');
           res.send(createTemplate(result.rows));
       }
   });
