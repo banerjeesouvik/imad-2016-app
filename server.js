557bf3b09ae3a4770b8f53a71f33e1de1918d018
Souvik Banerjee <banerjeesouvik@users.noreply.github.com>
2016-11-06 00:31:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/test-db', function (req, res) {
   });
 });
 
-app.get('/test-db2', function (req, res) {
+app.get('/poems', function (req, res) {
   pool.query('SELECT title,body,dop,poet_name FROM poems inner join poet on poems.poet_id=poet.poet_id order by dop asc', function(err,result) {
       if(err){
           res.status(500).send(err.toString());
@@ -102,9 +102,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/poems',function(req, res) {
+/*app.get('/poems',function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'poems.html'));
-});
+});*/
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
