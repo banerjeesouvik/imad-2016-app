adae9957aad83490515eaea21ffd9fab62837dd0
Souvik Banerjee <banerjeesouvik@users.noreply.github.com>
2016-11-18 11:21:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(session({
 	secret:'jaydenleone',
 	resave:true,
 	saveUninitialized:true,
-	cookie:{ maxAge: 1000*60*60*24*7}
+	cookie:{ maxAge: 1000*60*60*24*7,secure:true}
 }));
 
 var pool=new Pool(config);
