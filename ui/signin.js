d490787cd940b7d4872178db95d8ac990da9fa93
Souvik Banerjee <banerjeesouvik@users.noreply.github.com>
2016-11-20 16:30:24 +0530

    Update signin.js

--- ui/signin.js
+++ ui/signin.js
@@ -8,7 +8,7 @@ login_form_submit.onclick= function(){
  request.onreadystatechange= function(){
 		if(request.readyState===XMLHttpRequest.DONE){
 			if(request.status===200)
-			  window.location.replace('http://localhost:8080/profile');
+			  window.location.replace('http://banerjeesouvik.imad.hasura-app.io/profile');
 			else if(request.status===500)
 			  alert('Something went wrong. Please try again.');
 			else if(request.status===403){
