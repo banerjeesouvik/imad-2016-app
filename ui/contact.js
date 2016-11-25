aa70cbfbfc4926669ee021ad01b957e1ec5b387c
Souvik Banerjee <banerjeesouvik@users.noreply.github.com>
2016-11-25 09:55:52 +0530

    [imad-console] Updates ui/contact.js

--- ui/contact.js
+++ ui/contact.js
@@ -5,3 +5,17 @@ var souvik=document.getElementById("myimg");
 		souvik.onmouseout=function(){
 			souvik.setAttribute('src',"https://scontent.fccu1-1.fna.fbcdn.net/v/t1.0-9/14463110_1159333654159776_5140025532760958800_n.jpg?oh=8581b563cef8dba7d7318a418abe4f8b&oe=58B15325");
 };
+
+function checksession(){
+	var sesreq=new XMLHttpRequest();
+	sesreq.onreadystatechange=function(){
+	    if(sesreq.readyState===XMLHttpRequest.DONE){
+		if(sesreq.status===200)
+		  window.location.href="http://banerjeesouvik.imad.hasura-app.io/profile";
+	    }
+	}
+	sesreq.open('GET','http://banerjeesouvik.imad.hasura-app.io/checksession',true);
+	sesreq.send(null);
+}
+
+checksession();
