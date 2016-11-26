526220b46788dd9faa28faeed49b19b8c5887da1
Souvik Banerjee <banerjeesouvik@users.noreply.github.com>
2016-11-26 21:30:18 +0530

    Add files via upload

---
+++ ui/poemlikes.js
@@ -0,0 +1,45 @@
+var like=document.getElementById("lk_btn");
+var uid=document.getElementById("user_id");
+var pid=document.getElementById("poem_id");
+
+
+like.onclick=function(){
+	var xhr=new XMLHttpRequest();
+	xhr.onreadystatechange=function(){
+		if(xhr.readyState===XMLHttpRequest.DONE){
+			if(xhr.status===200){
+			  alert('Thanks for liking!');
+			  like.innerHTML="You liked this poem";
+			  like.setAttribute('title',"You liked this poem");
+			  like.setAttribute('style',"cursor:text");
+			  window.location.reload(true);
+			}
+			else if(xhr.status===500)
+			  alert('You already liked this poem');
+		}
+
+	}
+	xhr.open('POST',"http://localhost:8080/like",true);
+	xhr.setRequestHeader('Content-Type','application/json');
+	xhr.send(JSON.stringify({usr_id:uid.value,pm_id:pid.value}));
+};
+
+
+function checklike(){
+  var xhr=new XMLHttpRequest();
+	xhr.onreadystatechange=function(){
+		if(xhr.readyState===XMLHttpRequest.DONE){
+			if(xhr.status===200){
+			  like.innerHTML="You liked this poem";
+			  like.setAttribute('title',"You liked this poem");
+			  like.setAttribute('style',"cursor:text");
+			}
+		}
+
+	}
+	xhr.open('POST',"http://localhost:8080/check-like",true);
+	xhr.setRequestHeader('Content-Type','application/json');
+	xhr.send(JSON.stringify({u:uid.value,p:pid.value}));
+}
+
+checklike();
